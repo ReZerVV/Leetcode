1887d6dccbe9abe6cd9cdb8ec2008d5b39a4e6aa
kiril <kirillmorozov547@gmail.com>
2023-08-23 23:47:04 +0300

    add majority element solution

---
+++ Leetcode.Solutions/MajorityElement.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode.Solutions;
+
+/*
+    Given an array nums of size n, return the majority element.
+
+    The majority element is the element that appears more than ⌊n / 2⌋ times.
+    You may assume that the majority element always exists in the array.
+ */
+
+public static class MajorityElement
+{
+    public static int Solution(int[] nums)
+    {
+        Dictionary<int, int> numsCount = new Dictionary<int, int>();
+
+        for (int numsIndex = 0; numsIndex < nums.Length; ++numsIndex)
+        {
+            if (numsCount.ContainsKey(nums[numsIndex]))
+            {
+                numsCount[nums[numsIndex]]++;
+            }
+            else
+            {
+                numsCount.Add(nums[numsIndex], 1);
+            }
+        }
+
+        int maxNum = nums[0];
+        foreach (int key in numsCount.Keys)
+        {
+            if (numsCount[maxNum] < numsCount[key])
+            {
+                maxNum = key;
+            }
+        }
+        
+        return maxNum;
+    }
+}
---
+++ Leetcode.Test/MajorityElementTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Leetcode.Test;
+
+[TestClass]
+public class MajorityElementTest
+{
+    [TestMethod]
+    public void MajorityElementTestCase1()
+    {
+        int[] nums = new int[] { 3, 2, 3 };
+
+        int val = MajorityElement.Solution(nums);
+        
+        Assert.AreEqual(3, val);
+    }
+    
+    [TestMethod]
+    public void MajorityElementTestCase2()
+    {
+        int[] nums = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+
+        int val = MajorityElement.Solution(nums);
+        
+        Assert.AreEqual(2, val);
+    }
+}
